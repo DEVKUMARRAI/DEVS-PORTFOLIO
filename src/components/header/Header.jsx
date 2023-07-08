890b896205f2019d5e1c506ddaf29b848782d5a4
DEVKUMARRAI <91594658+DEVKUMARRAI@users.noreply.github.com>
2023-07-08 22:47:19 +0530

    Header Section Completed And Made Responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import profile_pic from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <div>
-       Header
+       <header>
+          <div className="container header_container">
+            <h5>Hey ! Welcome To My Portfolio.</h5>
+            <h1>Dev Kumar Rai</h1>
+            <h5 className="text-light">Full Stack Developer</h5>
+            <CTA/>
+            <HeaderSocials/>
+            <div className="me">
+              <img src={profile_pic} alt="" />
+            </div>
+            <a href="#contact" className='scroll_down'>Scroll Down</a>
+          </div>
+      
+       </header>
     </div>
   )
 }
