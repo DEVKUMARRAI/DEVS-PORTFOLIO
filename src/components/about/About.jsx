5cf5e0563074ad531b101240c2774fd05bcb3ee7
DEVKUMARRAI <91594658+DEVKUMARRAI@users.noreply.github.com>
2023-07-09 03:31:30 +0530

    About Section Completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import image from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const about = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2 className="aboutme">About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={image} alt="About Image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
 
-export default about
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>20+ Clients Served Till Date</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>20+ Projetcs</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+            sapiente delectus nihil excepturi assumenda atque qui repudiandae
+            vero quia saepe est porro, architecto fugit eius exercitationem unde
+            nulla optio eum nostrum temporibus pariatur obcaecati?
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default about;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaFacebook} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div  className='header_socials'>
-      <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com" target="_blank"><FaGithub/></a>
+      <a href="https://www.linkedin.com/in/dev-rai-a81183224/" target="_blank"><BsLinkedin/></a>
+      <a href="https://github.com/DEVKUMARRAI" target="_blank"><FaGithub/></a>
       <a href="https://facebook.com" target="_blank"><FaFacebook/></a>
     </div>
   )
